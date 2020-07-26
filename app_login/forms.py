baae8a14165d90c26db91e81c9493dd37d2bee80
tuhindewan <tuhinsshadow@gmail.com>
2020-07-26 12:36:51 +0600

    User Profile:   - UpdateProfile form class create

--- app_login/forms.py
+++ app_login/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
@@ -9,3 +9,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UpdateProfile(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
